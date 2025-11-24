d8c46bd7c40505d07c74ccb500fde33cde15dcaa
dishant1123 <dishantshah1417@gmail.com>
2025-11-24 19:40:49 +0530

    ...

---
+++ 24nov.cpp
@@ -0,0 +1,21 @@
+// print  heelo world  using namespace std; 
+/*
+#include<iostream>  
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    cout<<"ram";
+    return 0; 
+}
+*/ 
+
+// without using  namespace std 
+
+#include<iostream>
+int main()
+{
+    std::cout<<"hello world";
+    std::cout<<"ram";
+    return 0; 
+}
